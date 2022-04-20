6f7c1f86e619278e0f5c894e2075a91dc78af561
zh-betina <zh.betina@gmail.com>
2022-04-20 10:38:18 +0200

    Fill in missing document info for db

--- Services/UploadFileService.cs
+++ Services/UploadFileService.cs
@@ -1,6 +1,4 @@
-using System.Net.Http.Headers;
 using Healthometer_API.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -36,6 +34,22 @@ public class UploadFileService
         if (Directory.Exists(userDir)) return;
         Directory.CreateDirectory(userDir);
     }
+
+    private Document PrepareDocInfoForDatabase(Document docInfo, string format, string path)
+    {
+        var docToSendToDb = new Document
+        {
+            Id = ObjectId.GenerateNewId().ToString(),
+            Format = format,
+            Path = path,
+            Date = docInfo.Date,
+            Category = docInfo.Category,
+            Name = docInfo.Name,
+            Status = docInfo.Status
+        };
+
+        return docToSendToDb;
+    }
     public async Task<string> OnPostUploadAsync(string userId, IFormFile docFile, Document docInfo)
     {
         CreateDirectoryForUser(userId);
@@ -43,13 +57,17 @@ public class UploadFileService
         var randomName = Path.GetRandomFileName();
         var folderName = Path.Combine("Docs", userId, randomName);
         var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+        var fileExtension = Path.GetExtension(docFile.FileName);
         
         if (docFile.Length > 0)
         {
             await using var stream = new FileStream(pathToSave, FileMode.Create);
             docFile.CopyTo(stream);
 
-            await _documentsService.PutAsync(userId, docInfo);
+            var docInfoForDatabase = PrepareDocInfoForDatabase(docInfo, fileExtension, pathToSave);
+            
+            await _documentsService.PutAsync(userId, docInfoForDatabase);
         
             return "ok";
         }
